79e45c0c1de37e5c371b4c66ab691b07660d7c84
Ghcat0528 <fcatalanog7@gmail.com>
2025-03-04 08:55:47 -0700

    please work

--- capstone/vite.config.js
+++ capstone/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
-    port: process.env.PORT || 5173,  
-    host: '0.0.0.0',  
+    proxy: {
+      '/api': 'https://capstone-backend-url.onrender.com', 
+    },
+    host: true, 
   },
-});
+})

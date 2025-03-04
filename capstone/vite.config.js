01c15aea6b45ac5444bc6aadf2e38c502f483c44
Ghcat0528 <fcatalanog7@gmail.com>
2025-03-04 09:02:26 -0700

    correct link?

--- capstone/vite.config.js
+++ capstone/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://capstone-backend-url.onrender.com', 
+      '/api': 'https://capstone-backend-ax50.onrender.com', 
     },
     host: true, 
   },

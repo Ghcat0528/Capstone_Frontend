b2d53539fe275e2541015db7c1d77eb8bb6655fd
Ghcat0528 <fcatalanog7@gmail.com>
2025-03-04 10:13:56 -0700

    i need to sleep its been 2 days

--- capstone/src/assets/pgs/Auth.jsx
+++ capstone/src/assets/pgs/Auth.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import '../../../src/Auth.css'
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL 
+
 
 const Auth = () => {
   const navigate = useNavigate();
--- capstone/vite.config.js
+++ capstone/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'https://capstone-backend-ax50.onrender.com',
-    },
     host: true,
     allowedHosts: ['capstone-frontend-wctm.onrender.com'],
   },

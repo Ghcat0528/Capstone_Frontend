bfe9318d7f3e9bd69fc6dcd85f698b789a1c2215
Ghcat0528 <fcatalanog7@gmail.com>
2025-03-04 09:28:48 -0700

    please god

--- capstone/vite.config.js
+++ capstone/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://capstone-backend-ax50.onrender.com', 
+      '/api': 'https://capstone-backend-ax50.onrender.com',
     },
-    host: true, 
-    allowedHosts: ['capstone-frontend-wctm.onrender.com']
+    host: true,
+    allowedHosts: ['capstone-frontend-wctm.onrender.com'],
   },
-})
+});
